1f6b71a45291afebdd21710ee38628102c181a41
Данила Копицын <supermuper543@mail.ru>
2024-09-09 13:42:27 +0300

    Добавьте файлы проекта.

---
+++ homework1/homework1.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+using namespace homework1;
+int main()
+{
+    puts("   _____||____");
+    puts("  /    _  _   \\");
+    puts(" /   /     \\   \\");
+    puts("/___/_______\\___\\");
+    puts("|   |   _   |   |");
+    puts("|   |  | |  |   |");
+    puts("|___|__|_|__|___|");
+}
